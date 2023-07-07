8e0155ec2759c5c3699a6ba4013e29643ca6d584
xiaoguan <37170228+guanwenjie@users.noreply.github.com>
2023-07-07 11:33:52 +0800

    Update home.tsx

--- app/components/home.tsx
+++ app/components/home.tsx
@@ -27,7 +27,6 @@ import { SideBar } from "./sidebar";
 import { useAppConfig } from "../store/config";
 import { AuthPage } from "./auth";
 import { getClientConfig } from "../config/client";
-import { api } from "../client/api";
 
 export function Loading(props: { noLogo?: boolean }) {
   return (
@@ -153,21 +152,8 @@ function Screen() {
   );
 }
 
-export function useLoadData() {
-  const config = useAppConfig();
-
-  useEffect(() => {
-    (async () => {
-      const models = await api.llm.models();
-      config.mergeModels(models);
-    })();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-}
-
 export function Home() {
   useSwitchTheme();
-  useLoadData();
 
   useEffect(() => {
     console.log("[Config] got config from build time", getClientConfig());
